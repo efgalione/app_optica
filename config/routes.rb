39385cf6d62b9df9837fdf8ff07d3785701032f2
Esteban Fernandez <efgalione@gmail.com>
2015-12-20 13:10:33 -0200

    views fixes and rails_admin
    -integration with new attributes to the views
    -add gem file admin_rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   resources :recipes
   resources :recipes
   # The priority is based upon order of creation: first created -> highest priority.
